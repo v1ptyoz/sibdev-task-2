a7eff96ead9ca6cadbf3ccd71dde0575d3ff3874
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-02-11 22:20:29 +1000

    router for app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Login from './pages/Login';
 import Main from './pages/Main';
 import { Routes, Route } from "react-router-dom";
+import Favorites from './pages/Favorites';
+import Results from './pages/Results';
+import Search from './pages/Search';
 
 
 function App() {
@@ -9,7 +12,11 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="login" element={<Login />} />
-        <Route path="/" element={<Main /> } />
+        <Route path="/" element={<Main /> }>
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="results" element={<Results />} />
+          <Route path="search" element={<Search />} />
+        </Route>
     </Routes>
     </div>
   );
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,15 +1,23 @@
 import { Layout } from 'antd';
-import Search from '../Search';
 import SiteHeader from '../../components/SiteHeader';
-
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 const { Content } = Layout;
 
 export function Main() {
+  const location = useLocation();
+  const navigate = useNavigate()
+
+  useEffect(() =>{
+    if (location.pathname === "/")
+     navigate("search")
+  }, [])
+
   return (
     <>
       <SiteHeader />
       <Content style={{ width: '100%', height: '100%', background: '#FAFAFA' }}>
-        <Search />
+        <Outlet />
       </Content>
     </>
   )
