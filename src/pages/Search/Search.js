5c61ce54e2045a5920cfece2056a685d2aea9143
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-02-11 14:05:51 +1000

    add search page

---
+++ src/pages/Search/Search.js
@@ -0,0 +1,10 @@
+import "./Search.css"
+import SearchForm from "../../components/SearchForm";
+
+export function Search() {
+  return(
+    <div className="search">
+      <SearchForm />
+    </div>
+  )
+}
