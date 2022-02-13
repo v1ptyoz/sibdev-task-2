95c6b27e1939d2baa5ae19915be18cb4e410bc3a
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-02-13 12:09:28 +1000

    add search handler

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,21 +1,48 @@
 import "./SearchForm.css"
 import { Form, Input, Button } from 'antd';
 import { HeartOutlined, HeartTwoTone } from '@ant-design/icons';
+import { useDispatch } from "react-redux";
+import { getVideos } from "../../api/getVideos";
+import { AddFavorite } from "../../store/favorites/actions";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { saveFavorites } from "../../api/saveFavorites";
 
 export function SearchForm(props) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const q = useSelector(state => state.videos.query);
+  const loading = useSelector(state => state.videos.loading);
+  const favorites = useSelector(state => state.favorites.list);
+  
+
+  const inFavorite = !!favorites.filter(item => item.query === q).length;
+
+
+  const searchHandler = async (query) => {
+    await dispatch(getVideos(query));
+    navigate("/results", { replace: true });
+  }
+
+  const addToFavorite = async () => {
+    await dispatch(AddFavorite({query: q}))
+    await dispatch(saveFavorites());
+  }
+
   return (
     <div className="search-form">
       {props.children}
-      <Form layout="inline" className="search-form__form">
-        <Form.Item name="search" style={{ width: "100%", marginRight: "0", flex: "1" }} >
+      <Form layout="inline" className="search-form__form" onFinish={searchHandler}>
+        <Form.Item name="query" style={{ width: "100%", marginRight: "0", flex: "1" }} initialValue={q}>
           <Input
             placeholder="Что хотите посмотреть?"
             suffix={props.withIcon && (
-              props.inFavorite ? <HeartTwoTone /> : <HeartOutlined />)}
+              inFavorite ? <HeartTwoTone /> : <HeartOutlined onClick={addToFavorite}/>
+              )}
           >
           </Input>
         </Form.Item>
-        <Button type="primary" htmlType="submit">Найти</Button>
+        <Button type="primary" htmlType="submit" loading={loading}>Найти</Button>
       </Form>
     </div>
   )
