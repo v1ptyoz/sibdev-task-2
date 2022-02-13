8ba5a401c55b82ce4749bf78fc22e47d1cea4ad6
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-02-13 12:07:36 +1000

    add favorite item logic

--- src/components/FavoriteItem/FavoriteItem.js
+++ src/components/FavoriteItem/FavoriteItem.js
@@ -1,25 +1,45 @@
 import "./FavoriteItem.css"
 import React, { useState } from 'react';
 import FavoriteItemModal from "../FavoriteItemModal";
+import { Button } from "antd";
+import { EditFilled, CaretRightFilled } from '@ant-design/icons';
+import { getVideos } from "../../api/getVideos";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-export function FavoriteItem() {
+
+export function FavoriteItem({query, deep, name, sortBy}) {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [requestDeep, setRequestDeep] = useState(deep);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const showModal = () => {
     setIsModalVisible(true);
   };
 
+  const search = async () => {
+    await dispatch(getVideos({query, deep: requestDeep}));
+    navigate("/results");
+  }
+
 
   return (
-    <>
-      <p className="favorite__item" onClick={showModal}>
-        sdfadsfasdf
-      </p>
+    <div className="favorite__item">
+      {query}
+      <div className="favorite__controls">
+      <Button onClick={showModal}><EditFilled /></Button>
+      <Button type="primary" onClick={search}><CaretRightFilled /></Button>
+      </div>
       <FavoriteItemModal 
         showModal={isModalVisible} 
         onCancel={() => setIsModalVisible(false)} 
-        onSave={() => setIsModalVisible(false)}
+        query={query}
+        deep={deep}
+        name={name}
+        sortBy={sortBy}
+        setRequestDeep={setRequestDeep}
         />
-    </>
+    </div>
   )
 }
