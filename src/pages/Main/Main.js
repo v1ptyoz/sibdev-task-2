273a59c7f15df8f15a0410afd4fbcd41edcbba9a
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-02-13 12:10:09 +1000

    load favorites from localstorage

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,11 +2,19 @@ import { Layout } from 'antd';
 import SiteHeader from '../../components/SiteHeader';
 import { Outlet, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { loadFavorites } from '../../api/loadFavorites';
 const { Content } = Layout;
 
 export function Main() {
   const isLoggedIn = useSelector(state => state.user.isLoggedIn);
+  const name = useSelector(state => state.user.username)
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(loadFavorites(name));
+  }, [])
 
   return isLoggedIn ? (
     <>
