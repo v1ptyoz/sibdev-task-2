ef1d85f1364bdefc47c811abc9da1263de83ecfe
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-02-13 12:10:47 +1000

    if no results - go to search, style switcher

--- src/pages/Results/Results.js
+++ src/pages/Results/Results.js
@@ -5,24 +5,25 @@ import Controls from "../../components/Controls";
 import Cards from "../../components/Cards";
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-
+import { useState } from "react";
 
 const { Title } = Typography;
 
 export function Results() {
-  const isLoggedIn = useSelector(state => state.user.isLoggedIn);
+  const results = useSelector(state => state.videos.list);
+  const [grid, setGrid] = useState(false)
 
-  return isLoggedIn ? (
+  return results.length !== 0 ? (
     <div className="container">
       <div className="results">
         <SearchForm withIcon={true}>
           <Title level={3}>Поиск видео</Title>
         </SearchForm>
-        <Controls />
-        <Cards grid={true}/>
+        <Controls grid={grid} setGrid={setGrid}/>
+        <Cards grid={grid}/>
       </div>
     </div>
   ) : (
-    <Navigate to="login" />
+    <Navigate to="/search" />
   )
 }
